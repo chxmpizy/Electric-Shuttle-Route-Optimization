0fc883c79d1ed76af5b3976ea1156dd477b8e553
champNewbie <rattasat0012@gmail.com>
2026-03-28 11:44:57 +0700

    chore: delete some unused folder and move main system into optimization folder

---
+++ simulation/Passenger.py
@@ -0,0 +1,5 @@
+class Passenger:
+    def __init__(self, origin, destination, arrival_time):
+        self.origin = origin
+        self.destination = destination
+        self.arrival_time = arrival_time
---
+++ simulation/queue.py
@@ -0,0 +1,19 @@
+queues = {
+    "Dorm": [],
+    "Green": [],
+    "SC2_SC3": [],
+    "Health": [],
+    "Lecture": [],
+    "Terminal" : [],
+    "Convention" : [],
+    "Library" : [],
+    "SC1" : [],
+    "Gate1" : [],
+    "Hospital" : [],
+    "Social" : [],
+    "Park" : [],
+    "Dome" : []
+}
+
+waiting_times = []
+queue_lengths = []
